1a7f522b2d6169633c9109952415d918cb483639
j341nono <kanakana.nono1224@gmail.com>
2025-12-13 05:43:36 +0900

    feat: 文字列の結合や修了条件などを実装した

--- main.sh
+++ main.sh
@@ -2,7 +2,24 @@
 
 hako=""
 running=true
+
 while [ "$running" = true ]; do
     x=$((RANDOM % 2))
     
+    if [ "$x" = 0 ]; then
+        char="あ"
+    else
+        char="た"
+    fi
+
+    # -n をつけて改行しないようにする
+    echo -n "$char"
+
+    hako="${hako}${char}"
+
+    if [[ "$hako" =~ "あたたたた"$ ]]; then
+        echo ""
+        echo "お前はもう死んでいる"
+        running=false
+    fi
 done
